96285c3721b4186cc274f16844a6aab66384bdef
karandeepsingh-nexg <karandeepsingh@viewlift.com>
2022-01-24 00:26:45 +0530

    adding image

--- src/components/homeContent/HomeContent.js
+++ src/components/homeContent/HomeContent.js
@@ -12,7 +12,7 @@ const HomeContent = () => {
           <p> <span>SMART CITY </span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap </p>
       </div>
       <div class="images">
-                    {/* <img src={smartCity} class="shape" alt='' /> */}
+                    <img src={smartCity} class="shape" alt='' />
       </div>
   </div>
   <div className='pros-cons, home-content'>
